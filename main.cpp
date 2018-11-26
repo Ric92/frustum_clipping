c3df4e76409f38d995053b7cbd0527f8eb213a28
Ric92 <ricloplop@gmail.com>
2018-11-26 10:08:51 +0100

    CMakeLists with PCL, Eigen and OpenCV

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <Eigen/Eigen>
+#include <iomanip>
+#include <sstream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/plot.hpp>
+
+int main(int argc,char **argv){
+    std::cout << "Hello world \n";
+}
