767c1b1cf1e8df22c34c207340a34a097148253d
Ric92 <ricloplop@gmail.com>
2018-12-05 11:11:17 +0100

    Added volume calculation and fixed pcl compilation issues with c++

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,8 @@
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/console/parse.h>
 #include <pcl/filters/frustum_culling.h>
+#include <pcl/surface/convex_hull.h>
+
 
 #include "Clipper.h"
 #include "Drawer.h"
@@ -28,34 +30,49 @@ int main(int argc, char **argv)
     Clipper clip;
     Eigen::Matrix4f pose1;
     pose1 << 1, 0, 0, 0,
-             0, 1, 0, 0,
-             0, 0, 1, 0,
-             0, 0, 0, 1;
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1;
     std::shared_ptr<Frustum> f1(new Frustum(1, pose1, 45, 45, 1, 5));
     draw->frustum(f1);
 
     Eigen::Matrix4f pose2;
     pose2 << 1, 0, 0, 1,
-             0, 0.5, 0, 1,
-             0, 0, 1, 2,
-             0, 0, 0, 1;
+        0, 0.5, 0, 1,
+        0, 0, 1, 2,
+        0, 0, 0, 1;
     std::shared_ptr<Frustum> f2(new Frustum(2, pose2, 45, 45, 1, 5));
     draw->frustum(f2);
     std::vector<Eigen::Vector3f> inter;
-    clip.clipFrustumFrustum(f1,f2,inter);
-    clip.clipFrustumFrustum(f2,f1,inter);
-    
-    std::vector<double> frustumPoints;
+    clip.clipFrustumFrustum(f1, f2, inter);
+    clip.clipFrustumFrustum(f2, f1, inter);
 
+    std::vector<double> frustumPoints;
+    // Create a Convex Hull
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_hull(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::ConvexHull<pcl::PointXYZ> chull;
+    chull.setComputeAreaVolume(true);
     std::string v = "Intersection_point_";
     int i = 0;
-    for(auto point: inter){
-        draw->point(point,v+std::to_string(i));
+    for (auto point : inter)
+    {
+        draw->point(point, v + std::to_string(i));
         frustumPoints.push_back(point[0]);
         frustumPoints.push_back(point[1]);
         frustumPoints.push_back(point[2]);
+        pcl::PointXYZ newPoint;
+        newPoint.x = point[0];
+        newPoint.y = point[1];
+        newPoint.z = point[2];
+        cloud_hull->push_back(newPoint);
         i++;
     }
+
+    chull.setInputCloud(cloud_hull);
+    chull.reconstruct(*cloud_hull);
+
+    std::cout << "Convex hull has: " << cloud_hull->points.size() << " data points." << std::endl;
+    std::cout << "Convex hull has: " << chull.getTotalVolume() << " m³." << std::endl;
     // std::cout << frustum1->volume() << "\n";
     // //First test
     // Eigen::Vector3f v1(2, 0, 2);     //Right-Left line
