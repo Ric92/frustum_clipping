36753b1b24466b8ac1927afd964de61860135ba6
ric92 <ricloplop@gmail.com>
2020-03-10 00:53:55 +0100

    Added new polyhedron cube

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@
 
 #include "Drawer.h"
 #include "Frustum.h"
+#include "Cube.h"
 
 int main(int argc, char **argv)
 {
@@ -28,35 +29,35 @@ int main(int argc, char **argv)
         0, 1, 0, 0,
         0, 0, 1, 0,
         0, 0, 0, 1;
-    std::shared_ptr<Frustum> f1(new Frustum(1, pose1, 45, 45, 1, 5));
+    std::shared_ptr<Cube> c1(new Cube(1, pose1, 10, 10, 10));
 
     Eigen::Matrix4f pose2;
     pose2 << 1, 0, 0, 1,
         0, 0.5, 0, 1,
         0, 0, 1, 2,
         0, 0, 0, 1;
-    std::shared_ptr<Frustum> f2(new Frustum(2, pose2, 45, 45, 1, 5));
+    std::shared_ptr<Cube> c2(new Cube(2, pose2, 15, 5, 10));
 
-    draw->polyhedron(f1->id, f1->mPosition, f1->getFacets());
-    draw->polyhedron(f2->id, f2->mPosition, f2->getFacets());
+    draw->polyhedron(c1->id, c1->mPose.block(0, 3, 3, 1), c1->getFacets());
+    draw->polyhedron(c2->id, c2->mPose.block(0, 3, 3, 1), c2->getFacets());
 
-    std::vector<Eigen::Vector3f> inter;
-    f1->clipConvexPolyhedron(f2, inter);
-    std::cout << "Number of intersection points: " << inter.size() << "\n";
+    // std::vector<Eigen::Vector3f> inter;
+    // f1->clipConvexPolyhedron(f2, inter);
+    // std::cout << "Number of intersection points: " << inter.size() << "\n";
 
-    std::string v = "Intersection_point_";
-    int i = 0;
-    for (auto point : inter)
-    {
-        draw->point(point, v + std::to_string(i));
-        i++;
-    }
+    // std::string v = "Intersection_point_";
+    // int i = 0;
+    // for (auto point : inter)
+    // {
+    //     draw->point(point, v + std::to_string(i));
+    //     i++;
+    // }
 
-    std::cout << "Convex hull has " << f1->computeVolumeFromPoints(inter) << " m³ \n";
-    std::cout << "Volume of frustum: " << f1->getVolume() << " m³ \n";
-    std::cout << "% of volume intersected " << 100*(f1->computeVolumeFromPoints(inter)/f1->getVolume()) << "  \n";
+    // std::cout << "Convex hull has " << f1->computeVolumeFromPoints(inter) << " m³ \n";
+    // std::cout << "Volume of frustum: " << f1->getVolume() << " m³ \n";
+    // std::cout << "% of volume intersected " << 100*(f1->computeVolumeFromPoints(inter)/f1->getVolume()) << "  \n";
 
-    std::cout << "Clipping ended \n";
+    // std::cout << "Clipping ended \n";
     bool end = true;
     while (end)
     {
@@ -65,3 +66,51 @@ int main(int argc, char **argv)
     std::cout << "Closing \n";
     return (0);
 }
+
+// int main(int argc, char **argv)
+// {
+//     Drawer<pcl::PointXYZRGB>::init();
+//     auto draw = Drawer<pcl::PointXYZRGB>::get();
+
+//     Eigen::Matrix4f pose1;
+//     pose1 << 1, 0, 0, 0,
+//         0, 1, 0, 0,
+//         0, 0, 1, 0,
+//         0, 0, 0, 1;
+//     std::shared_ptr<Frustum> f1(new Frustum(1, pose1, 45, 45, 1, 5));
+
+//     Eigen::Matrix4f pose2;
+//     pose2 << 1, 0, 0, 1,
+//         0, 0.5, 0, 1,
+//         0, 0, 1, 2,
+//         0, 0, 0, 1;
+//     std::shared_ptr<Frustum> f2(new Frustum(2, pose2, 45, 45, 1, 5));
+
+//     draw->polyhedron(f1->id, f1->mPosition, f1->getFacets());
+//     draw->polyhedron(f2->id, f2->mPosition, f2->getFacets());
+
+//     std::vector<Eigen::Vector3f> inter;
+//     f1->clipConvexPolyhedron(f2, inter);
+//     std::cout << "Number of intersection points: " << inter.size() << "\n";
+
+//     std::string v = "Intersection_point_";
+//     int i = 0;
+//     for (auto point : inter)
+//     {
+//         draw->point(point, v + std::to_string(i));
+//         i++;
+//     }
+
+//     std::cout << "Convex hull has " << f1->computeVolumeFromPoints(inter) << " m³ \n";
+//     std::cout << "Volume of frustum: " << f1->getVolume() << " m³ \n";
+//     std::cout << "% of volume intersected " << 100*(f1->computeVolumeFromPoints(inter)/f1->getVolume()) << "  \n";
+
+//     std::cout << "Clipping ended \n";
+//     bool end = true;
+//     while (end)
+//     {
+//         draw->spinOnce();
+//     }
+//     std::cout << "Closing \n";
+//     return (0);
+// }
